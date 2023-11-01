cb1addb7d8e30a856781e643ce391048a6b31d2c
BagasSatriaYn <duhjvdedhvhjufahfy@gmail.com>
2023-11-01 14:13:26 +0700

    Linear

--- ArrayNilai09.java
+++ ArrayNilai09.java
@@ -2,18 +2,43 @@ import java.util.Scanner;
 
 public class ArrayNilai09 {
     public static void main(String[] args) {
-    Scanner input = new Scanner(System.in);
+        Scanner input = new Scanner(System.in);
 
-    int[] nilaiAkhir = new int[10];
-
-    for (int i = 0; i < 10; i++) {
-        System.out.print("Masukkan nilai akhir mahasiswa ke-" + (i+1) + " : ");
-        nilaiAkhir[i] = input.nextInt();
-    }
-
-    for (int i = 0; i < 10; i++) {
-        System.out.println("Nilai akhir mahasiswa ke-" + (i+1) + " adalah " + nilaiAkhir[i] );
+        int[] nilaiAkhir = new int[10];
+        double total = 0;
+        double nilaiTertinggi, nilaiTerendah;
         
+
+        for (int i = 0; i < nilaiAkhir.length; i++) {
+            System.out.print("Masukkan nilai akhir mahasiswa ke-" + (i + 1) + " : ");
+            nilaiAkhir[i] = input.nextInt();
+        }
+
+        for (int i = 0; i < 10; i++) {
+            if (nilaiAkhir[i] > 70) {
+                System.out.println("Nilai akhir mahasiswa ke-" + (i+1) + " lulus ");
+            } else  { 
+                System.out.println("Nilai akhir mahasiswa ke-" + (i+1) + " tidak lulus");
+
+            }
+
+        for (int i = 1; i < 10; i++) {
+            if (nilaiAkhir[i] > 70 ) {
+                nilaiTertinggi = nilaiAkhir[i];
+            }
+
+            if (nilaiAkhir[i] < nilaiTerendah) {
+                nilaiTerendah = nilaiAkhir[i];
+            }
+
+            total += nilaiAkhir[i];
+        }
+
+            double rataRata = (double) total / 10;
+
+            System.out.println(" Nilai tertinggi : " + nilaiTertinggi);
+            System.out.println("Nilai terendah : " + nilaiTerendah);
+            System.out.println(" Nilai rata-rata " + rataRata);
         }
     }
 }
--- coba.java
+++ coba.java
@@ -1,8 +1,28 @@
 import java.util.Scanner;
 
-public class coba {
+public class ArrayNilai09 {
     public static void main(String[] args) {
-        
-    num = 6
-    if num > 5:
+        Scanner input = new Scanner(System.in);
+
+        int[] nilaiAkhir = new int[10];
+        double total = 0;
+        double rataRata;
+
+        for (int i = 0; i < nilaiAkhir.length; i++) {
+            System.out.print("Masukkan nilai akhir mahasiswa ke-" + (i + 1) + " : ");
+            nilaiAkhir[i] = input.nextInt();
+        }
+
+        for (int i = 0; i < 10; i++) {
+            if (nilaiAkhir[i] > 70) {
+                System.out.println("Nilai akhir mahasiswa ke-" + (i+1) + " lulus ");
+            } else  { 
+                System.out.println("Nilai akhir mahasiswa ke-" + (i+1) + " tidak lulus");
+
+            }
+
+            rataRata = total/nilaiAkhir.length;
+            System.out.println("Rata-rata nilai = " +rataRata);
+        }
+    }
 }
